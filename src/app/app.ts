6206c7241462945e99f5dd9e9e1ea3023141a369
Charlie Briones <charliebriones@gmail.com>
2026-02-19 19:18:28 +0800

    remove loader, added render api url

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserService } from './core/services/user.service';
-import { LoadingScreenComponent } from './shared/loading-screen.component/loading-screen.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, LoadingScreenComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  loading = true;
-  userService = inject(UserService);
-
-  ngOnInit() {
-    // Call a lightweight ping endpoint to wake Azure Free Tier backend
-    this.userService.getUsers().subscribe({
-      next: () => (this.loading = false),
-      error: () => {
-        // Even if ping fails, hide after 5s to avoid blocking UI
-        setTimeout(() => (this.loading = false), 5000);
-      },
-    });
-  }
-}
+export class App {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://um-api-webapp-apbye4aufshpdyh5.southeastasia-01.azurewebsites.net', // Prod API URL
+  // apiBaseUrl: 'https://um-api-webapp-apbye4aufshpdyh5.southeastasia-01.azurewebsites.net', // Prod API URL // Azure API
+  apiBaseUrl: 'https://usermanagement-mbtm.onrender.com', // Prod API URL // Azure API
 };
